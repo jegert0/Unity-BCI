8fd92d59dcf8204043f63c63572cd96eccdc3553
jegert0 <jegertm@gmail.com>
2024-01-30 20:21:08 -0500

    added plugin

--- Unity BCI Car/Assets/Scripts/OmniCarController.cs
+++ Unity BCI Car/Assets/Scripts/OmniCarController.cs
@@ -7,18 +7,35 @@ public class OmniCarController : MonoBehaviour
 {
     public float speed = 20;
     public Vector3 _rotationSpeed = new Vector3 (0, 50, 0);
+    public bool headset = false;
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        Vector3 movementDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        transform.Translate(movementDirection * speed * Time.deltaTime);
+    {   if(!headset){
 
-        if(Input.GetKey("k")){
-            transform.Rotate(_rotationSpeed * Time.deltaTime);
+            Vector3 movementDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            //transform.Translate(movementDirection * speed * Time.deltaTime);
+            forwardBack(movementDirection);
+
+            if(Input.GetKey("k")){
+                transform.Rotate(_rotationSpeed * Time.deltaTime);
+            }
+            if(Input.GetKey("j")){
+                transform.Rotate( -_rotationSpeed * Time.deltaTime);
+            }
+
         }
-        if(Input.GetKey("j")){
-            transform.Rotate( -_rotationSpeed * Time.deltaTime);
+        else{
+            Vector3 dir = headsetParser();
+            forwardBack(dir);
         }
+    }
+
+    void forwardBack(Vector3 input){
+        transform.Translate(input * speed * Time.deltaTime);
+    }
+
+    Vector3 headsetParser(){
+        return new Vector3(1,0,0);
     }
 }
